9ee96fd6333d489036ca0b1b490ec82a582ddff8
huashenliang <JohnnyLiang77@gmail.com>
2020-03-29 10:23:33 -0500

    able to get data from the client using socket.io

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -1,6 +1,23 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import queryString from 'query-string';
+import io from 'socket.io-client';
 
-const Chat = () => {
+const Chat = ({location}) => {
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
+    const ENDPOINT ='localhost:5000';
+
+    useEffect (() => {
+        const {room, name} = queryString.parse(location.search)
+        
+        const socket = io(ENDPOINT);
+
+        setName(name);
+        setRoom(room);
+
+        socket.emit('join', {name, room})
+    }, [ENDPOINT, location.search])
+
     return ( 
         <div>
             Chat
